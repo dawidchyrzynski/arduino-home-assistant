bc4b7faef076362af21f6c51b24a9d135b979262
Dawid Chyrzynski <dawid.chyrzynski@gmail.com>
2020-12-05 00:23:16 +0100

    add led-switch example

---
+++ examples/led-switch/led-switch.ino
@@ -0,0 +1,39 @@
+#include <Ethernet.h>
+#include <ArduinoHA.h>
+
+#define LED_PIN 9
+
+byte mac[] = {0x00, 0x10, 0xFA, 0x6E, 0x38, 0x4A};
+
+EthernetClient client;
+HADevice device(mac, sizeof(mac));
+HAMqtt mqtt(client, device);
+HASwitch led("led", false, mqtt);
+
+void onSwitchStateChanged(bool state, HASwitch* s)
+{
+    digitalWrite(LED_PIN, (state ? HIGH : LOW));
+}
+
+void setup() {
+    pinMode(LED_PIN, OUTPUT);
+    digitalWrite(LED_PIN, LOW);
+
+    // you don't need to verify return status
+    Ethernet.begin(mac);
+
+    // set device's details (optional)
+    device.setName("Arduino");
+    device.setSoftwareVersion("1.0.0");
+
+    // handle switch state
+    led.onStateChanged(onSwitchStateChanged);
+
+    // please change IP address to yours
+    mqtt.begin(IPAddress(192,168,0,17));
+}
+
+void loop() {
+    Ethernet.maintain();
+    mqtt.loop();
+}
