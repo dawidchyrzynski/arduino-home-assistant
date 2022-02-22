3d802e2c9b2e046beef13f46fbad66c8e5f130f6
Alberto Bettin <albertobettin@live.com>
2022-02-22 23:38:05 +0100

    Minor fix in the implementation of Mqtt-number

---
+++ src/device-types/HANumber.cpp
@@ -0,0 +1,245 @@
+#ifdef ARDUINO_ARCH_SAMD
+#include <avr/dtostrf.h>
+#endif
+
+#include "HANumber.h"
+#ifdef ARDUINOHA_NUMBER
+
+#include "../HAMqtt.h"
+#include "../HADevice.h"
+
+HANumber::HANumber(const char* uniqueId) :
+    BaseDeviceType("number", uniqueId),
+    _units(nullptr),
+    _icon(nullptr),
+    _valueCallback(nullptr),
+    _retain(false),
+    _value(0),
+    _precision(0)
+{
+
+}
+
+HANumber::HANumber(const char* uniqueId, HAMqtt& mqtt) :
+    HANumber(uniqueId)
+{
+    (void)mqtt;
+}
+
+void HANumber::onMqttConnected()
+{
+    if (strlen(uniqueId()) == 0) {
+        return;
+    }
+
+    publishConfig();
+    publishAvailability();
+
+     if (!_retain) {
+        publishValue(_value);
+    }
+
+
+    DeviceTypeSerializer::mqttSubscribeTopic(
+        this,
+        DeviceTypeSerializer::CommandTopic
+    );
+
+}
+
+void HANumber::onMqttMessage(
+    const char* topic,
+    const uint8_t* payload,
+    const uint16_t& length
+)
+{
+    if (compareTopics(topic, DeviceTypeSerializer::CommandTopic)) {
+        char valueStr[length + 1];
+        memcpy(valueStr, payload, length);
+        valueStr[length] = 0;
+        float floatVal = atof(valueStr);  
+        setValue(floatVal, true);
+    }
+}
+
+
+bool HANumber::setValue(float value, uint8_t precision)
+{
+    _precision = precision;
+
+    if (publishValue(value)) {
+        _value = value;
+
+        if (_valueCallback) {
+            _valueCallback(value, this);
+        }
+
+        return true;
+    }
+
+    return false;
+
+}
+
+bool HANumber::publishValue(float fValue)
+{
+    uint8_t digitsNb = floor(log10(floor(fValue))) + 1;
+    char value[digitsNb + 3 + _precision]; // null terminator, dot, minus sign
+    dtostrf(fValue, 0, _precision, value);
+
+    if (strlen(uniqueId()) == 0 ) {
+        return false;
+    }
+
+
+    if (!mqtt()->isConnected()) {
+        return false;
+    }
+
+    const uint16_t& topicSize = DeviceTypeSerializer::calculateTopicLength(
+        componentName(),
+        uniqueId(),
+        DeviceTypeSerializer::StateTopic
+    );
+    if (topicSize == 0) {
+        return false;
+    }
+
+    char topic[topicSize];
+    DeviceTypeSerializer::generateTopic(
+        topic,
+        componentName(),
+        uniqueId(),
+        DeviceTypeSerializer::StateTopic
+    );
+
+    if (strlen(topic) == 0) {
+        return false;
+    }
+
+    return mqtt()->publish(topic, value, true);
+}
+
+uint16_t HANumber::calculateSerializedLength(
+    const char* serializedDevice
+) const
+{
+    if (serializedDevice == nullptr) {
+        return 0;
+    }
+
+    const HADevice* device = mqtt()->getDevice();
+    if (device == nullptr) {
+        return 0;
+    }
+
+    uint16_t size = 0;
+    size += DeviceTypeSerializer::calculateBaseJsonDataSize();
+    size += DeviceTypeSerializer::calculateNameFieldSize(getName());
+    size += DeviceTypeSerializer::calculateUniqueIdFieldSize(uniqueId());
+    size += DeviceTypeSerializer::calculateDeviceFieldSize(serializedDevice);
+    size += DeviceTypeSerializer::calculateAvailabilityFieldSize(this);
+
+      // cmd topic
+    {
+        const uint16_t& topicLength = DeviceTypeSerializer::calculateTopicLength(
+            componentName(),
+            uniqueId(),
+            DeviceTypeSerializer::CommandTopic,
+            false
+        );
+
+        if (topicLength == 0) {
+            return 0;
+        }
+
+        // Field format: "cmd_t":"[TOPIC]"
+        size += topicLength + 10; // 10 - length of the JSON decorators for this field
+    }
+
+    // state topic
+    {
+        const uint16_t& topicLength = DeviceTypeSerializer::calculateTopicLength(
+            componentName(),
+            uniqueId(),
+            DeviceTypeSerializer::StateTopic,
+            false
+        );
+
+        if (topicLength == 0) {
+            return 0;
+        }
+
+        // Field format: ,"stat_t":"[TOPIC]"
+        size += topicLength + 12; // 12 - length of the JSON decorators for this field
+    }
+
+    // units of measurement
+    if (_units != nullptr) {
+        // Format: ,"unit_of_meas":"[UNITS]"
+        size += strlen(_units) + 18; // 18 - length of the JSON decorators for this field
+    }
+
+    // icon
+    if (_icon != nullptr) {
+        // Field format: ,"ic":"[ICON]"
+        size += strlen(_icon) + 8; // 8 - length of the JSON decorators for this field
+    }
+
+    return size; // exludes null terminator
+}
+
+bool HANumber::writeSerializedData(const char* serializedDevice) const
+{
+    if (serializedDevice == nullptr) {
+        return false;
+    }
+
+    DeviceTypeSerializer::mqttWriteBeginningJson();
+
+    // command topic
+    {
+        static const char Prefix[] PROGMEM = {"\"cmd_t\":\""};
+        DeviceTypeSerializer::mqttWriteTopicField(
+            this,
+            Prefix,
+            DeviceTypeSerializer::CommandTopic
+        );
+    }
+
+    // state topic
+    {
+        static const char Prefix[] PROGMEM = {",\"stat_t\":\""};
+        DeviceTypeSerializer::mqttWriteTopicField(
+            this,
+            Prefix,
+            DeviceTypeSerializer::StateTopic
+        );
+    }
+
+
+    // units of measurement
+    if (_units != nullptr) {
+        static const char Prefix[] PROGMEM = {",\"unit_of_meas\":\""};
+        DeviceTypeSerializer::mqttWriteConstCharField(Prefix, _units);
+    }
+
+    // icon
+    if (_icon != nullptr) {
+        static const char Prefix[] PROGMEM = {",\"ic\":\""};
+        DeviceTypeSerializer::mqttWriteConstCharField(
+            Prefix,
+            _icon
+        );
+    }
+
+    DeviceTypeSerializer::mqttWriteNameField(getName());
+    DeviceTypeSerializer::mqttWriteUniqueIdField(uniqueId());
+    DeviceTypeSerializer::mqttWriteAvailabilityField(this);
+    DeviceTypeSerializer::mqttWriteDeviceField(serializedDevice);
+    DeviceTypeSerializer::mqttWriteEndJson();
+
+    return true;
+}
+
+#endif
---
+++ src/device-types/HANumber.h
@@ -0,0 +1,98 @@
+#ifndef AHA_HANumber_H
+#define AHA_HANumber_H
+
+#include "BaseDeviceType.h"
+
+
+
+#ifdef ARDUINOHA_NUMBER
+
+#define HANumber_CALLBACK(name) void (*name)(float, HANumber*)
+
+
+class HANumber : public BaseDeviceType
+{
+public:
+    /**
+     * Initializes binary sensor.
+     *
+     * @param uniqueId Unique ID of the sensor. Recommendes characters: [a-z0-9\-_]
+     */
+    HANumber(
+        const char* uniqueId
+    );
+    HANumber(
+        const char* uniqueId,
+        HAMqtt& mqtt
+    ); // legacy constructor
+
+    /**
+     * Publishes configuration of the sensor to the MQTT.
+     */
+    virtual void onMqttConnected() override;
+
+        /**
+     * Processes message received from the MQTT broker.
+     * The method updates state of the switch (if message matches switch'es topic).
+     */
+    virtual void onMqttMessage(
+        const char* topic,
+        const uint8_t* payload,
+        const uint16_t& length
+    ) override;
+
+    /**
+     * Publishes new value of the sensor.
+     * Please note that connection to MQTT broker must be acquired.
+     * Otherwise method will return false.
+     *
+     * @param state Value to publish.
+     * @returns Returns true if MQTT message has been published successfully.
+     */
+    bool setValue(float value, uint8_t precision = 2);
+
+
+    inline float getValue(){ return _value;}
+
+    /**
+     * Defines the units of measurement of the sensor, if any.
+     *
+     * @param units For example: °C, %
+     */
+    inline void setUnitOfMeasurement(const char* units)
+        { _units = units; }
+
+    /**
+     * Sets icon of the sensor, e.g. `mdi:home`.
+     *
+     * @param icon Material Design Icon name with mdi: prefix.
+     */
+    inline void setIcon(const char* icon)
+        { _icon = icon; }
+
+      /**
+     * Registers callback that will be called each time the state of the switch changes.
+     * Please note that it's not possible to register multiple callbacks for the same switch.
+     *
+     * @param callback
+     */
+    inline void onValueChanged(HANumber_CALLBACK(callback))
+        { _valueCallback = callback; }
+
+private:
+    bool publishValue(float value);
+    uint16_t calculateSerializedLength(const char* serializedDevice) const override;
+    bool writeSerializedData(const char* serializedDevice) const override;
+
+    float _value;
+    uint8_t _precision;
+    const char* _units;
+    const char* _icon;
+    bool _retain;
+
+    HANumber_CALLBACK(_valueCallback);
+
+};
+
+#endif
+#endif
