11a2e26b5c0660319d15c7f4c2eebc49e8c89568
Dawid Chyrzynski <dawid.chyrzynski@gmail.com>
2022-04-16 19:24:49 +0200

    finished topics generator tests

--- tests/SerializerTopicsTest/SerializerTopicsTest.ino
+++ tests/SerializerTopicsTest/SerializerTopicsTest.ino
@@ -6,6 +6,11 @@ using aunit::TestRunner;
 
 EthernetClient dummyClient;
 const char DummyProgmemStr[] PROGMEM = {"dummyProgmem"};
+char tmpBuffer[128];
+
+void clearTmpBuffer() {
+    memset(tmpBuffer, 0, sizeof(tmpBuffer));
+}
 
 test(SerializerTopicsTest, calculate_config_no_mqtt) {
     HAMqtt::clearInstance();
@@ -112,6 +117,112 @@ test(SerializerTopicsTest, calculate_config_with_null_terminator) {
     );
 }
 
+test(SerializerTopicsTest, generate_config_no_mqtt) {
+    HAMqtt::clearInstance();
+    clearTmpBuffer();
+
+    uint16_t generatedLength = HASerializer::generateConfigTopic(
+        tmpBuffer,
+        "componentName",
+        "objectId"
+    );
+
+    // it should return 0 if there is no HAMqtt instance (singleton)
+    assertEqual(
+        (uint16_t)0,
+        generatedLength
+    );
+    assertTrue(strlen(tmpBuffer) == 0);
+}
+
+test(SerializerTopicsTest, generate_config_invalid_component) {
+    HAMqtt::clearInstance();
+    HADevice device("testDevice");
+    HAMqtt mqtt(dummyClient, device);
+    clearTmpBuffer();
+
+    uint16_t generatedLength = HASerializer::generateConfigTopic(
+        tmpBuffer,
+        nullptr,
+        "objectId"
+    );
+
+    // it should return 0 if componentName is null
+    assertEqual(
+        (uint16_t)0,
+        generatedLength
+    );
+    assertTrue(strlen(tmpBuffer) == 0);
+}
+
+test(SerializerTopicsTest, generate_config_invalid_object) {
+    HAMqtt::clearInstance();
+    HADevice device("testDevice");
+    HAMqtt mqtt(dummyClient, device);
+    clearTmpBuffer();
+
+    uint16_t generatedLength = HASerializer::generateConfigTopic(
+        tmpBuffer,
+        "componentName",
+        nullptr
+    );
+
+    // it should return 0 if objectId is null
+    assertEqual(
+        (uint16_t)0,
+        generatedLength
+    );
+    assertTrue(strlen(tmpBuffer) == 0);
+}
+
+test(SerializerTopicsTest, generate_config_invalid_prefix) {
+    HAMqtt::clearInstance();
+    HADevice device("testDevice");
+    HAMqtt mqtt(dummyClient, device);
+    mqtt.setDiscoveryPrefix(nullptr);
+    clearTmpBuffer();
+
+    uint16_t generatedLength = HASerializer::generateConfigTopic(
+        tmpBuffer,
+        "componentName",
+        "objectId"
+    );
+
+    // it should return 0 if discovery prefix is null
+    assertEqual(
+        (uint16_t)0,
+        generatedLength
+    );
+    assertTrue(strlen(tmpBuffer) == 0);
+}
+
+test(SerializerTopicsTest, generate_config) {
+    const char* deviceId = "testDevice";
+    const char* discoveryPrefix = "discoveryPrefix";
+    const char* componentName = "componentName";
+    const char* objectId = "objectId";
+    const char* expectedTopic = "discoveryPrefix/componentName/testDevice/objectId/config";
+
+    HAMqtt::clearInstance();
+    HADevice device(deviceId);
+    HAMqtt mqtt(dummyClient, device);
+    mqtt.setDiscoveryPrefix(discoveryPrefix);
+    clearTmpBuffer();
+
+    uint16_t generatedLength = HASerializer::generateConfigTopic(
+        tmpBuffer,
+        componentName,
+        objectId
+    );
+
+    // it should generate valid topic
+    assertEqual(
+        (uint16_t)strlen(expectedTopic) + 1,
+        generatedLength
+    );
+    assertTrue(strcmp(tmpBuffer, expectedTopic) == 0);
+}
+
 test(SerializerTopicsTest, calculate_data_no_mqtt) {
     HAMqtt::clearInstance();
 
@@ -120,7 +231,7 @@ test(SerializerTopicsTest, calculate_data_no_mqtt) {
         (uint16_t)0,
         HASerializer::calculateDataTopicLength(
             "objectId",
-            "topicP"
+            DummyProgmemStr
         )
     );
 }
@@ -131,7 +242,7 @@ test(SerializerTopicsTest, calculate_data_invalid_topic) {
     HAMqtt mqtt(dummyClient, device);
     mqtt.setDataPrefix("dataPrefix");
 
-    // it should return 0 if there is no HAMqtt instance (singleton)
+    // it should return 0 if topicP is null
     assertEqual(
         (uint16_t)0,
         HASerializer::calculateDataTopicLength(
@@ -147,12 +258,12 @@ test(SerializerTopicsTest, calculate_data_invalid_prefix) {
     HAMqtt mqtt(dummyClient, device);
     mqtt.setDataPrefix(nullptr);
 
-    // it should return 0 if there is no HAMqtt instance (singleton)
+    // it should return 0 if data prefix is null
     assertEqual(
         (uint16_t)0,
         HASerializer::calculateDataTopicLength(
             "objectId",
-            "topicP"
+            DummyProgmemStr
         )
     );
 }
@@ -199,6 +310,117 @@ test(SerializerTopicsTest, calculate_data_partial_with_null_terminator) {
             true
         )
     );
+}
+
+test(SerializerTopicsTest, generate_data_no_mqtt) {
+    HAMqtt::clearInstance();
+    clearTmpBuffer();
+
+    uint16_t generatedLength = HASerializer::generateDataTopic(
+        tmpBuffer,
+        "objectId",
+        DummyProgmemStr
+    );
+
+    // it should return 0 if there is no HAMqtt instance (singleton)
+    assertEqual(
+        (uint16_t)0,
+        generatedLength
+    );
+    assertTrue(strlen(tmpBuffer) == 0);
+}
+
+test(SerializerTopicsTest, generate_data_invalid_topic) {
+    HAMqtt::clearInstance();
+    HADevice device("testDevice");
+    HAMqtt mqtt(dummyClient, device);
+    clearTmpBuffer();
+
+    uint16_t generatedLength = HASerializer::generateDataTopic(
+        tmpBuffer,
+        "objectId",
+        nullptr
+    );
+
+    // it should return 0 if topicP is null
+    assertEqual(
+        (uint16_t)0,
+        generatedLength
+    );
+    assertTrue(strlen(tmpBuffer) == 0);
+}
+
+test(SerializerTopicsTest, generate_data_invalid_prefix) {
+    HAMqtt::clearInstance();
+    HADevice device("testDevice");
+    HAMqtt mqtt(dummyClient, device);
+    mqtt.setDataPrefix(nullptr);
+    clearTmpBuffer();
+
+    uint16_t generatedLength = HASerializer::generateDataTopic(
+        tmpBuffer,
+        "objectId",
+        DummyProgmemStr
+    );
+
+    // it should return 0 if data prefix is null
+    assertEqual(
+        (uint16_t)0,
+        generatedLength
+    );
+    assertTrue(strlen(tmpBuffer) == 0);
+}
+
+test(SerializerTopicsTest, generate_data_partial) {
+    const char* deviceId = "testDevice";
+    const char* dataPrefix = "dataPrefix";
+    const char* objectId = nullptr;
+    const char* expectedTopic = "dataPrefix/testDevice/dummyProgmem";
+
+    HAMqtt::clearInstance();
+    HADevice device(deviceId);
+    HAMqtt mqtt(dummyClient, device);
+    mqtt.setDataPrefix(dataPrefix);
+    clearTmpBuffer();
+
+    uint16_t generatedLength = HASerializer::generateDataTopic(
+        tmpBuffer,
+        objectId,
+        DummyProgmemStr
+    );
+
+    // it should generate valid partial data topic (without objectId)
+    assertEqual(
+        (uint16_t)strlen(expectedTopic) + 1,
+        generatedLength
+    );
+    assertTrue(strcmp(tmpBuffer, expectedTopic) == 0);
+}
+
+test(SerializerTopicsTest, generate_data_full) {
+    const char* deviceId = "testDevice";
+    const char* dataPrefix = "dataPrefix";
+    const char* objectId = "objectId";
+    const char* expectedTopic = "dataPrefix/testDevice/objectId/dummyProgmem";
+
+    HAMqtt::clearInstance();
+    HADevice device(deviceId);
+    HAMqtt mqtt(dummyClient, device);
+    mqtt.setDataPrefix(dataPrefix);
+    clearTmpBuffer();
+
+    uint16_t generatedLength = HASerializer::generateDataTopic(
+        tmpBuffer,
+        objectId,
+        DummyProgmemStr
+    );
+
+    // it should generate valid full data topic
+    assertEqual(
+        (uint16_t)strlen(expectedTopic) + 1,
+        generatedLength
+    );
+    assertTrue(strcmp(tmpBuffer, expectedTopic) == 0);
 }
 
 void setup()
