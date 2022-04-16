d5e62310816d0c1990882419f52cae7a3197b6bb
Dawid Chyrzynski <dawid.chyrzynski@gmail.com>
2022-04-16 18:57:34 +0200

    add ArduinoHATests.h header

---
+++ src/ArduinoHATests.h
@@ -0,0 +1,18 @@
+#ifndef AHA_ARDUINOHATESTS_H
+#define AHA_ARDUINOHATESTS_H
+
+#include "HADevice.h"
+#include "HAMqtt.h"
+#include "HAUtils.h"
+#include "utils/HADictionary.h"
+#include "utils/HASerializer.h"
+#include "device-types/HABinarySensor.h"
+#include "device-types/HACover.h"
+#include "device-types/HAFan.h"
+#include "device-types/HAHVAC.h"
+#include "device-types/HASensor.h"
+#include "device-types/HASwitch.h"
+#include "device-types/HATagScanner.h"
+#include "device-types/HATriggers.h"
+
+#endif
