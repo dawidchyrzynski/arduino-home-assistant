7c2f67ee3320e12e3c3f8f298d0b2f61c6989c8a
Dawid Chyrzynski <dawid.chyrzynski@gmail.com>
2022-06-04 15:18:34 +0200

    fix ArduinoHA.h includes for tests

--- src/ArduinoHA.h
+++ src/ArduinoHA.h
@@ -4,8 +4,14 @@
 #include "HADevice.h"
 #include "HAMqtt.h"
 #include "HAUtils.h"
-#include "mocks/PubSubClientMock.h"
 #include "device-types/HABinarySensor.h"
 #include "device-types/HAButton.h"
+#include "device-types/HACover.h"
+
+#ifdef ARDUINOHA_TEST
+#include "mocks/PubSubClientMock.h"
+#include "utils/HADictionary.h"
+#include "utils/HASerializer.h"
+#endif
 
 #endif
