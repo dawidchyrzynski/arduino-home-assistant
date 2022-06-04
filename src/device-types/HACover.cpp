e0cd8a1c042e03e355d049bf086c6304a4e084bd
Dawid Chyrzynski <dawid.chyrzynski@gmail.com>
2022-06-04 15:05:12 +0200

    implemented HACover using a new framework

---
+++ src/device-types/HACover.cpp
@@ -0,0 +1,171 @@
+#include "HACover.h"
+#ifndef EX_ARDUINOHA_COVER
+
+#include "../HAMqtt.h"
+#include "../HAUtils.h"
+#include "../utils/HASerializer.h"
+
+HACover::HACover(const char* uniqueId) :
+    BaseDeviceType("cover", uniqueId),
+    _commandCallback(nullptr),
+    _currentState(StateUnknown),
+    _currentPosition(0),
+    _class(nullptr),
+    _icon(nullptr),
+    _retain(false)
+{
+
+}
+
+bool HACover::setState(const CoverState state, const bool force)
+{
+    if (!force && _currentState == state) {
+        return true;
+    }
+
+    if (publishState(state)) {
+        _currentState = state;
+        return true;
+    }
+
+    return false;
+}
+
+bool HACover::setPosition(const int16_t position, const bool force)
+{
+    if (!force && _currentPosition == position) {
+        return true;
+    }
+
+    if (publishPosition(position)) {
+        _currentPosition = position;
+        return true;
+    }
+
+    return false;
+}
+
+
+void HACover::buildSerializer()
+{
+    if (_serializer || !uniqueId()) {
+        return;
+    }
+
+    _serializer = new HASerializer(this);
+    _serializer->set(HANameProperty, _name);
+    _serializer->set(HAUniqueIdProperty, _uniqueId);
+    _serializer->set(HADeviceClassProperty, _class);
+    _serializer->set(HAIconProperty, _icon);
+
+    // optional property
+    if (_retain) {
+        _serializer->set(HARetainProperty, &_retain, HASerializer::BoolPropertyType);
+    }
+
+    _serializer->set(HASerializer::WithDevice);
+    _serializer->set(HASerializer::WithAvailability);
+    _serializer->topic(HAStateTopic);
+    _serializer->topic(HACommandTopic);
+    _serializer->topic(HAPositionTopic);
+}
+
+void HACover::onMqttConnected()
+{
+    if (!uniqueId()) {
+        return;
+    }
+
+    publishConfig();
+    publishAvailability();
+
+    if (!_retain) {
+        publishState(_currentState);
+        publishPosition(_currentPosition);
+    }
+
+    subscribeTopic(HACommandTopic);
+}
+
+void HACover::onMqttMessage(
+    const char* topic,
+    const uint8_t* payload,
+    const uint16_t length
+)
+{
+    if (_commandCallback && checkTopic(topic, HACommandTopic)) {
+        char cmd[length + 1];
+        memset(cmd, 0, sizeof(cmd));
+        memcpy(cmd, payload, length);
+        handleCommand(cmd);
+    }
+}
+
+bool HACover::publishState(CoverState state)
+{
+    if (!uniqueId() || state == StateUnknown) {
+        return false;
+    }
+
+    const char *stateP = nullptr;
+    switch (state) {
+        case StateClosed:
+            stateP = HAClosedState;
+            break;
+
+        case StateClosing:
+            stateP = HAClosingState;
+            break;
+
+        case StateOpen:
+            stateP = HAOpenState;
+            break;
+
+        case StateOpening:
+            stateP = HAOpeningState;
+            break;
+
+        case StateStopped:
+            stateP = HAStoppedState;
+            break;
+
+        default:
+            return false;
+    }
+
+    return publishOnDataTopic(HAStateTopic, stateP, false, true);
+}
+
+bool HACover::publishPosition(int16_t position)
+{
+    if (!uniqueId()) {
+        return false;
+    }
+
+    uint8_t size = HAUtils::calculateNumberSize(position);
+    if (size == 0) {
+        return false;
+    }
+
+    char str[size + 1]; // with null terminator
+    HAUtils::numberToStr(str, position);
+
+    return publishOnDataTopic(HAPositionTopic, str);
+}
+
+void HACover::handleCommand(const char* cmd)
+{
+    if (!_commandCallback) {
+        return;
+    }
+
+    if (strcmp_P(cmd, HACloseCommand) == 0) {
+        _commandCallback(CommandClose, this);
+    } else if (strcmp_P(cmd, HAOpenCommand) == 0) {
+        _commandCallback(CommandOpen, this);
+    } else if (strcmp_P(cmd, HAStopCommand) == 0) {
+        _commandCallback(CommandStop, this);
+    }
+}
+
+#endif
---
+++ src/device-types/HACover.h
@@ -0,0 +1,131 @@
+#ifndef AHA_HACOVER_H
+#define AHA_HACOVER_H
+
+#include "BaseDeviceType.h"
+
+#ifndef EX_ARDUINOHA_COVER
+
+#define HACOVER_CALLBACK(name) void (*name)(CoverCommand cmd, HACover* cover)
+
+class HACover : public BaseDeviceType
+{
+public:
+    enum CoverState {
+        StateUnknown = 0,
+        StateClosed,
+        StateClosing,
+        StateOpen,
+        StateOpening,
+        StateStopped
+    };
+
+    enum CoverCommand {
+        CommandOpen,
+        CommandClose,
+        CommandStop
+    };
+
+    HACover(const char* uniqueId);
+
+    /**
+     * Changes state of the cover and publishes MQTT message.
+     * Please note that if a new value is the same as previous one,
+     * the MQTT message won't be published.
+     *
+     * @param state New state of the cover.
+     * @param force Forces to update state without comparing it to previous known state.
+     * @returns Returns true if MQTT message has been published successfully.
+     */
+    bool setState(const CoverState state, const bool force = false);
+
+    /**
+     * Sets current state of the cover without pushing the state to Home Assistant.
+     * This method may be useful if you want to change state before connection
+     * with MQTT broker is acquired.
+     *
+     * @param state New state of the cover.
+     */
+    inline void setCurrentState(const CoverState state)
+        { _currentState = state; }
+
+    /**
+     * Changes position of the cover and publishes MQTT message.
+     * Please note that if a new value is the same as previous one,
+     * the MQTT message won't be published.
+     *
+     * @param position New position of the cover.
+     * @param force Forces to update state without comparing it to previous known state.
+     * @returns Returns true if MQTT message has been published successfully.
+     */
+    bool setPosition(const int16_t position, const bool force = false);
+
+    /**
+     * Sets current position of the cover without pushing the value to Home Assistant.
+     * This method may be useful if you want to change position before connection
+     * with MQTT broker is acquired.
+     *
+     * @param position New position of the cover.
+     */
+    inline void setCurrentPosition(const int16_t position)
+        { _currentPosition = position; }
+
+    /**
+     * Sets class of the device.
+     * You can find list of available values here: https://www.home-assistant.io/integrations/cover/
+     * 
+     * @param class Class name
+     */
+    virtual void setDeviceClass(const char* deviceClass)
+        { _class = deviceClass; }
+
+    /**
+     * Sets icon of the sensor.
+     * Any icon from MaterialDesignIcons.com. Prefix name with mdi:, ie mdi:home.
+     *
+     * @param class Icon name
+     */
+    virtual void setIcon(const char* icon)
+        { _icon = icon; }
+
+    /**
+     * Sets `retain` flag for commands published by Home Assistant.
+     * By default it's set to false.
+     *
+     * @param retain
+     */
+    inline void setRetain(const bool retain)
+        { _retain = retain; }
+
+    /**
+     * Registers callback that will be called each time the command from HA is received.
+     * Please note that it's not possible to register multiple callbacks for the same covers.
+     *
+     * @param callback
+     */
+    inline void onCommand(HACOVER_CALLBACK(callback))
+        { _commandCallback = callback; }
+
+protected:
+    virtual void buildSerializer() override;
+    virtual void onMqttConnected() override;
+    virtual void onMqttMessage(
+        const char* topic,
+        const uint8_t* payload,
+        const uint16_t length
+    ) override;
+
+private:
+    bool publishState(const CoverState state);
+    bool publishPosition(const int16_t position);
+    void handleCommand(const char* cmd);
+
+    HACOVER_CALLBACK(_commandCallback);
+    CoverState _currentState;
+    int16_t _currentPosition;
+    const char* _class;
+    const char* _icon;
+    bool _retain;
+};
+
+#endif
+#endif
