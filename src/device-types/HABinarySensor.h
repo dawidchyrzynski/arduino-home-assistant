6a6a85fd22e5032d3ad3c6dabd86c09654f9f879
Dawid Chyrzynski <dawid.chyrzynski@gmail.com>
2022-08-31 22:41:30 +0200

    HABinarySensor docs

--- src/device-types/HABinarySensor.h
+++ src/device-types/HABinarySensor.h
@@ -5,6 +5,13 @@
 
 #ifndef EX_ARDUINOHA_BINARY_SENSOR
 
+/**
+ * HABinarySensor represents a binary sensor that allows publishing on/off state to the Home Assistant panel.
+ *
+ * @note
+ * You can find more information about this entity in the Home Assistant documentation:
+ * https://www.home-assistant.io/integrations/binary_sensor.mqtt/
+ */
 class HABinarySensor : public HABaseDeviceType
 {
 public:
@@ -14,35 +21,33 @@ public:
      * Sets class of the device.
      * You can find list of available values here: https://www.home-assistant.io/integrations/binary_sensor/#device-class
      * 
-     * @param class Class name
+     * @param class The class name.
      */
     inline void setDeviceClass(const char* deviceClass)
         { _class = deviceClass; }
 
     /**
      * Sets icon of the sensor.
-     * Any icon from MaterialDesignIcons.com. Prefix name with mdi:, ie mdi:home.
+     * Any icon from MaterialDesignIcons.com (for example: "mdi:home").
      *
-     * @param class Icon name
+     * @param class The icon name.
      */
     inline void setIcon(const char* icon)
         { _icon = icon; }
 
     /**
-     * Changes state of the sensor and publishes MQTT message.
-     * Please note that if a new value is the same as previous one,
-     * the MQTT message won't be published.
+     * Changes state of the sensor and publish MQTT message.
+     * Please note that if a new value is the same as the previous one the MQTT message won't be published.
      *
-     * @param state New state of the sensor.
-     * @param force Forces to update state without comparing it to previous known state.
-     * @returns Returns true if MQTT message has been published successfully.
+     * @param state New state of the sensor (true - on, false - off).
+     * @param force Forces to update state without comparing it to a previous known state.
+     * @returns Returns true if the MQTT message has been published successfully.
      */
     bool setState(const bool state, const bool force = false);
 
     /**
-     * Sets current state of the sensor without publishing it to Home Assistant.
-     * This method may be useful if you want to change state before connection
-     * with MQTT broker is acquired.
+     * Sets the current state of the sensor without publishing it to Home Assistant.
+     * This method may be useful if you want to change the state before the connection with the MQTT broker is acquired.
      *
      * @param state New state of the sensor.
      */
@@ -51,7 +56,6 @@ public:
 
     /**
      * Returns last known state of the sensor.
-     * If setState method wasn't called the initial value will be returned.
      */
     inline bool getCurrentState() const
         { return _currentState; }
@@ -61,10 +65,20 @@ protected:
     virtual void onMqttConnected() override;
 
 private:
+    /**
+     * Publishes the MQTT message with the given state.
+     * 
+     * @returns Returns true if the MQTT message has been published successfully.
+     */
     bool publishState(bool state);
 
+    /// Device class. It can be nullptr.
     const char* _class;
+
+    /// Icon of the sensor. It can be nullptr.
     const char* _icon;
+
+    /// Current state of the sensor. By default it's false.
     bool _currentState;
 };
 
