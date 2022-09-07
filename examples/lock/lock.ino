fc18ea5c04d420ca7d239d91717e9eae4af4e37f
Dawid Chyrzynski <dawid.chyrzynski@gmail.com>
2022-09-07 12:33:51 +0200

    added HALock example

---
+++ examples/lock/lock.ino
@@ -0,0 +1,52 @@
+#include <Ethernet.h>
+#include <ArduinoHA.h>
+
+#define BROKER_ADDR     IPAddress(192,168,0,17)
+
+byte mac[] = {0x00, 0x10, 0xFA, 0x6E, 0x38, 0x4A};
+
+EthernetClient client;
+HADevice device(mac, sizeof(mac));
+HAMqtt mqtt(client, device);
+
+// "myLock" is unique ID of the lock. You should define your own ID.
+HALock lock("myLock");
+
+void onLockCommand(HALock::LockCommand cmd) {
+    if (cmd == HALock::CommandLock) {
+        Serial.println("Command: Lock");
+        lock.setState(HALock::StateLocked); // report state back to the HA
+    } else if (cmd == HALock::CommandUnlock) {
+        Serial.println("Command: Unlock");
+        lock.setState(HALock::StateUnlocked); // report state back to the HA
+    } else if (cmd == HALock::CommandOpen) {
+        if (lock.getCurrentState() != HALock::StateUnlocked) {
+            return; // optionally you can verify if the lock is unlocked before opening
+        }
+
+        Serial.println("Command: Open");
+    }
+}
+
+void setup() {
+    Serial.begin(9600);
+    Ethernet.begin(mac);
+
+    lock.onCommand(onLockCommand);
+    lock.setName("My door lock"); // optional
+    lock.setIcon("mdi:home"); // optional
+
+    // Optionally you can set retain flag for the HA commands
+    // lock.setRetain(true);
+
+    // Optionally you can enable optimistic mode for the HALock.
+    // In this mode you won't need to report state back to the HA when commands are executed.
+    // lock.setOptimistic(true);
+
+    mqtt.begin(BROKER_ADDR);
+}
+
+void loop() {
+    Ethernet.maintain();
+    mqtt.loop();
+}
