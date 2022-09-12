ab006444710b59c968b868573d347effce5fb1e7
Dawid Chyrzynski <dawid.chyrzynski@gmail.com>
2022-09-12 22:46:41 +0200

    update docs

--- docs/_static/custom.js
+++ docs/_static/custom.js
@@ -2,4 +2,12 @@ window.addEventListener('load', function() {
     document.querySelectorAll('a.reference.external').forEach(function(link) {
         link.target = '_blank';
     });
+
+    document.querySelectorAll('.footer a').forEach(function(link) {
+        link.target = '_blank';
+    });
+
+    document.querySelectorAll('form input[type="text"]').forEach(function(input) {
+        input.setAttribute("placeholder", "Search docs");
+    });
 });
