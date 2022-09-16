2b64f7badd9569563395b5f6aef6f552171fc1c8
Dawid Chyrzynski <dawid.chyrzynski@gmail.com>
2022-09-16 17:29:20 +0200

    tests for HANumber

--- tests/NumberTest/NumberTest.ino
+++ tests/NumberTest/NumberTest.ino
@@ -42,6 +42,369 @@ AHA_TEST(NumberTest, invalid_unique_id) {
     assertTrue(serializer == nullptr);
 }
 
+AHA_TEST(NumberTest, default_params) {
+    prepareTest
+
+    HANumber number(testUniqueId);
+    assertEntityConfig(
+        mock,
+        number,
+        "{\"uniq_id\":\"uniqueNumber\",\"dev\":{\"ids\":\"testDevice\"},\"stat_t\":\"testData/testDevice/uniqueNumber/stat_t\",\"cmd_t\":\"testData/testDevice/uniqueNumber/cmd_t\"}"
+    )
+    assertEqual(2, mock->getFlushedMessagesNb()); // config + default state
+}
+
+AHA_TEST(NumberTest, command_subscription) {
+    prepareTest
+
+    HANumber number(testUniqueId);
+    mqtt.loop();
+
+    assertEqual(1, mock->getSubscriptionsNb());
+    assertEqual(AHATOFSTR(CommandTopic), mock->getSubscriptions()[0]->topic);
+}
+
+AHA_TEST(NumberTest, availability) {
+    prepareTest
+
+    HANumber number(testUniqueId);
+    number.setAvailability(true);
+    mqtt.loop();
+
+    // availability is published after config in HANumber
+    assertMqttMessage(
+        1,
+        F("testData/testDevice/uniqueNumber/avty_t"),
+        "online",
+        true
+    )
+}
+
+AHA_TEST(NumberTest, publish_state_none) {
+    prepareTest
+
+    HANumber number(testUniqueId);
+    mqtt.loop();
+
+    assertEqual(2, mock->getFlushedMessagesNb());
+    assertMqttMessage(1, AHATOFSTR(StateTopic), "None", true)
+}
+
+AHA_TEST(NumberTest, publish_last_known_state_p0) {
+    prepareTest
+
+    HANumber number(testUniqueId);
+    number.setCurrentState(50);
+    mqtt.loop();
+
+    assertEqual(2, mock->getFlushedMessagesNb());
+    assertMqttMessage(1, AHATOFSTR(StateTopic), "50", true)
+}
+
+AHA_TEST(NumberTest, publish_last_known_state_p1) {
+    prepareTest
+
+    HANumber number(testUniqueId, HANumber::PrecisionP1);
+    number.setCurrentState(50);
+    mqtt.loop();
+
+    assertEqual(2, mock->getFlushedMessagesNb());
+    assertMqttMessage(1, AHATOFSTR(StateTopic), "50.0", true)
+}
+
+AHA_TEST(NumberTest, publish_last_known_state_p2) {
+    prepareTest
+
+    HANumber number(testUniqueId, HANumber::PrecisionP2);
+    number.setCurrentState(8888);
+    mqtt.loop();
+
+    assertEqual(2, mock->getFlushedMessagesNb());
+    assertMqttMessage(1, AHATOFSTR(StateTopic), "8888.00", true)
+}
+
+AHA_TEST(NumberTest, publish_last_known_state_p3) {
+    prepareTest
+
+    HANumber number(testUniqueId, HANumber::PrecisionP3);
+    number.setCurrentState(1.234);
+    mqtt.loop();
+
+    assertEqual(2, mock->getFlushedMessagesNb());
+    assertMqttMessage(1, AHATOFSTR(StateTopic), "1.234", true)
+}
+
+AHA_TEST(NumberTest, publish_nothing_if_retained) {
+    prepareTest
+
+    HANumber number(testUniqueId);
+    number.setRetain(true);
+    number.setCurrentState(50);
+    mqtt.loop();
+
+    assertEqual(1, mock->getFlushedMessagesNb()); // only config should be pushed
+}
+
+AHA_TEST(NumberTest, name_setter) {
+    prepareTest
+
+    HANumber number(testUniqueId);
+    number.setName("testName");
+
+    assertEntityConfig(
+        mock,
+        number,
+        "{\"name\":\"testName\",\"uniq_id\":\"uniqueNumber\",\"dev\":{\"ids\":\"testDevice\"},\"stat_t\":\"testData/testDevice/uniqueNumber/stat_t\",\"cmd_t\":\"testData/testDevice/uniqueNumber/cmd_t\"}"
+    )
+}
+
+AHA_TEST(NumberTest, device_class) {
+    prepareTest
+
+    HANumber number(testUniqueId);
+    number.setDeviceClass("testClass");
+
+    assertEntityConfig(
+        mock,
+        number,
+        "{\"uniq_id\":\"uniqueNumber\",\"dev_cla\":\"testClass\",\"dev\":{\"ids\":\"testDevice\"},\"stat_t\":\"testData/testDevice/uniqueNumber/stat_t\",\"cmd_t\":\"testData/testDevice/uniqueNumber/cmd_t\"}"
+    )
+}
+
+AHA_TEST(NumberTest, icon_setter) {
+    prepareTest
+
+    HANumber number(testUniqueId);
+    number.setIcon("testIcon");
+
+    assertEntityConfig(
+        mock,
+        number,
+        "{\"uniq_id\":\"uniqueNumber\",\"ic\":\"testIcon\",\"dev\":{\"ids\":\"testDevice\"},\"stat_t\":\"testData/testDevice/uniqueNumber/stat_t\",\"cmd_t\":\"testData/testDevice/uniqueNumber/cmd_t\"}"
+    )
+}
+
+AHA_TEST(NumberTest, retain_setter) {
+    prepareTest
+
+    HANumber number(testUniqueId);
+    number.setRetain(true);
+
+    assertEntityConfig(
+        mock,
+        number,
+        "{\"uniq_id\":\"uniqueNumber\",\"ret\":true,\"dev\":{\"ids\":\"testDevice\"},\"stat_t\":\"testData/testDevice/uniqueNumber/stat_t\",\"cmd_t\":\"testData/testDevice/uniqueNumber/cmd_t\"}"
+    )
+}
+
+AHA_TEST(NumberTest, optimistic_setter) {
+    prepareTest
+
+    HANumber number(testUniqueId);
+    number.setOptimistic(true);
+
+    assertEntityConfig(
+        mock,
+        number,
+        "{\"uniq_id\":\"uniqueNumber\",\"opt\":true,\"dev\":{\"ids\":\"testDevice\"},\"stat_t\":\"testData/testDevice/uniqueNumber/stat_t\",\"cmd_t\":\"testData/testDevice/uniqueNumber/cmd_t\"}"
+    )
+}
+
+AHA_TEST(NumberTest, mode_setter_box) {
+    prepareTest
+
+    HANumber number(testUniqueId);
+    number.setMode(HANumber::ModeBox);
+
+    assertEntityConfig(
+        mock,
+        number,
+        "{\"uniq_id\":\"uniqueNumber\",\"mode\":\"box\",\"dev\":{\"ids\":\"testDevice\"},\"stat_t\":\"testData/testDevice/uniqueNumber/stat_t\",\"cmd_t\":\"testData/testDevice/uniqueNumber/cmd_t\"}"
+    )
+}
+
+AHA_TEST(NumberTest, mode_setter_slider) {
+    prepareTest
+
+    HANumber number(testUniqueId);
+    number.setMode(HANumber::ModeSlider);
+
+    assertEntityConfig(
+        mock,
+        number,
+        "{\"uniq_id\":\"uniqueNumber\",\"mode\":\"slider\",\"dev\":{\"ids\":\"testDevice\"},\"stat_t\":\"testData/testDevice/uniqueNumber/stat_t\",\"cmd_t\":\"testData/testDevice/uniqueNumber/cmd_t\"}"
+    )
+}
+
+AHA_TEST(NumberTest, unit_of_measurement_setter) {
+    initMqttTest(testDeviceId)
+
+    HANumber number(testUniqueId);
+    number.setUnitOfMeasurement("%");
+
+    assertEntityConfig(
+        mock,
+        number,
+        "{\"uniq_id\":\"uniqueNumber\",\"unit_of_meas\":\"%\",\"dev\":{\"ids\":\"testDevice\"},\"stat_t\":\"testData/testDevice/uniqueNumber/stat_t\",\"cmd_t\":\"testData/testDevice/uniqueNumber/cmd_t\"}"
+    )
+}
+
+AHA_TEST(NumberTest, min_setter_p0) {
+    initMqttTest(testDeviceId)
+
+    HANumber number(testUniqueId);
+    number.setMin(2);
+
+    assertEntityConfig(
+        mock,
+        number,
+        "{\"uniq_id\":\"uniqueNumber\",\"min\":2,\"dev\":{\"ids\":\"testDevice\"},\"stat_t\":\"testData/testDevice/uniqueNumber/stat_t\",\"cmd_t\":\"testData/testDevice/uniqueNumber/cmd_t\"}"
+    )
+}
+
+AHA_TEST(NumberTest, min_setter_p1) {
+    initMqttTest(testDeviceId)
+
+    HANumber number(testUniqueId, HANumber::PrecisionP1);
+    number.setMin(2.5);
+
+    assertEntityConfig(
+        mock,
+        number,
+        "{\"uniq_id\":\"uniqueNumber\",\"min\":2.5,\"dev\":{\"ids\":\"testDevice\"},\"stat_t\":\"testData/testDevice/uniqueNumber/stat_t\",\"cmd_t\":\"testData/testDevice/uniqueNumber/cmd_t\"}"
+    )
+}
+
+AHA_TEST(NumberTest, min_setter_p2) {
+    initMqttTest(testDeviceId)
+
+    HANumber number(testUniqueId, HANumber::PrecisionP2);
+    number.setMin(95467.5);
+
+    assertEntityConfig(
+        mock,
+        number,
+        "{\"uniq_id\":\"uniqueNumber\",\"min\":95467.50,\"dev\":{\"ids\":\"testDevice\"},\"stat_t\":\"testData/testDevice/uniqueNumber/stat_t\",\"cmd_t\":\"testData/testDevice/uniqueNumber/cmd_t\"}"
+    )
+}
+
+AHA_TEST(NumberTest, min_setter_p3) {
+    initMqttTest(testDeviceId)
+
+    HANumber number(testUniqueId, HANumber::PrecisionP3);
+    number.setMin(50.5);
+
+    assertEntityConfig(
+        mock,
+        number,
+        "{\"uniq_id\":\"uniqueNumber\",\"min\":50.500,\"dev\":{\"ids\":\"testDevice\"},\"stat_t\":\"testData/testDevice/uniqueNumber/stat_t\",\"cmd_t\":\"testData/testDevice/uniqueNumber/cmd_t\"}"
+    )
+}
+
+AHA_TEST(NumberTest, max_setter_p0) {
+    initMqttTest(testDeviceId)
+
+    HANumber number(testUniqueId);
+    number.setMax(2);
+
+    assertEntityConfig(
+        mock,
+        number,
+        "{\"uniq_id\":\"uniqueNumber\",\"max\":2,\"dev\":{\"ids\":\"testDevice\"},\"stat_t\":\"testData/testDevice/uniqueNumber/stat_t\",\"cmd_t\":\"testData/testDevice/uniqueNumber/cmd_t\"}"
+    )
+}
+
+AHA_TEST(NumberTest, max_setter_p1) {
+    initMqttTest(testDeviceId)
+
+    HANumber number(testUniqueId, HANumber::PrecisionP1);
+    number.setMax(2.5);
+
+    assertEntityConfig(
+        mock,
+        number,
+        "{\"uniq_id\":\"uniqueNumber\",\"max\":2.5,\"dev\":{\"ids\":\"testDevice\"},\"stat_t\":\"testData/testDevice/uniqueNumber/stat_t\",\"cmd_t\":\"testData/testDevice/uniqueNumber/cmd_t\"}"
+    )
+}
+
+AHA_TEST(NumberTest, max_setter_p2) {
+    initMqttTest(testDeviceId)
+
+    HANumber number(testUniqueId, HANumber::PrecisionP2);
+    number.setMax(95467.5);
+
+    assertEntityConfig(
+        mock,
+        number,
+        "{\"uniq_id\":\"uniqueNumber\",\"max\":95467.50,\"dev\":{\"ids\":\"testDevice\"},\"stat_t\":\"testData/testDevice/uniqueNumber/stat_t\",\"cmd_t\":\"testData/testDevice/uniqueNumber/cmd_t\"}"
+    )
+}
+
+AHA_TEST(NumberTest, max_setter_p3) {
+    initMqttTest(testDeviceId)
+
+    HANumber number(testUniqueId, HANumber::PrecisionP3);
+    number.setMax(50.5);
+
+    assertEntityConfig(
+        mock,
+        number,
+        "{\"uniq_id\":\"uniqueNumber\",\"max\":50.500,\"dev\":{\"ids\":\"testDevice\"},\"stat_t\":\"testData/testDevice/uniqueNumber/stat_t\",\"cmd_t\":\"testData/testDevice/uniqueNumber/cmd_t\"}"
+    )
+}
+
+AHA_TEST(NumberTest, step_setter_p0) {
+    initMqttTest(testDeviceId)
+
+    HANumber number(testUniqueId);
+    number.setStep(2);
+
+    assertEntityConfig(
+        mock,
+        number,
+        "{\"uniq_id\":\"uniqueNumber\",\"step\":2,\"dev\":{\"ids\":\"testDevice\"},\"stat_t\":\"testData/testDevice/uniqueNumber/stat_t\",\"cmd_t\":\"testData/testDevice/uniqueNumber/cmd_t\"}"
+    )
+}
+
+AHA_TEST(NumberTest, step_setter_p1) {
+    initMqttTest(testDeviceId)
+
+    HANumber number(testUniqueId, HANumber::PrecisionP1);
+    number.setStep(2.5);
+
+    assertEntityConfig(
+        mock,
+        number,
+        "{\"uniq_id\":\"uniqueNumber\",\"step\":2.5,\"dev\":{\"ids\":\"testDevice\"},\"stat_t\":\"testData/testDevice/uniqueNumber/stat_t\",\"cmd_t\":\"testData/testDevice/uniqueNumber/cmd_t\"}"
+    )
+}
+
+AHA_TEST(NumberTest, step_setter_p2) {
+    initMqttTest(testDeviceId)
+
+    HANumber number(testUniqueId, HANumber::PrecisionP2);
+    number.setStep(0.01);
+
+    assertEntityConfig(
+        mock,
+        number,
+        "{\"uniq_id\":\"uniqueNumber\",\"step\":0.01,\"dev\":{\"ids\":\"testDevice\"},\"stat_t\":\"testData/testDevice/uniqueNumber/stat_t\",\"cmd_t\":\"testData/testDevice/uniqueNumber/cmd_t\"}"
+    )
+}
+
+AHA_TEST(NumberTest, step_setter_p3) {
+    initMqttTest(testDeviceId)
+
+    HANumber number(testUniqueId, HANumber::PrecisionP3);
+    number.setStep(0.001);
+
+    assertEntityConfig(
+        mock,
+        number,
+        "{\"uniq_id\":\"uniqueNumber\",\"step\":0.001,\"dev\":{\"ids\":\"testDevice\"},\"stat_t\":\"testData/testDevice/uniqueNumber/stat_t\",\"cmd_t\":\"testData/testDevice/uniqueNumber/cmd_t\"}"
+    )
+}
+
 void setup()
 {
     delay(1000);
