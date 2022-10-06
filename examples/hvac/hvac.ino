fea3476b4189c778d6f5b832beef0f83b32b8243
Dawid Chyrzynski <dawid.chyrzynski@gmail.com>
2022-10-06 20:04:13 +0200

    add HVAC example

---
+++ examples/hvac/hvac.ino
@@ -0,0 +1,112 @@
+#include <ESP8266WiFi.h>
+#include <ArduinoHA.h>
+
+#define BROKER_ADDR     IPAddress(192,168,0,17)
+#define WIFI_SSID       "MyNetwork"
+#define WIFI_PASSWORD   "MyPassword"
+
+WiFiClient client;
+HADevice device;
+HAMqtt mqtt(client, device);
+
+// By default HAHVAC supports only reporting of the temperature.
+// You can enable feature you need using the second argument of the constructor.
+// Please check the documentation of the HAHVAC class.
+HAHVAC hvac(
+  "my_name",
+  HAHVAC::TargetTemperatureFeature | HAHVAC::PowerFeature | HAHVAC::ModesFeature
+);
+
+unsigned long lastTempPublishAt = 0;
+double lastTemp = 0;
+
+void onTargetTemperatureCommand(HAUtils::Number temperature, uint8_t precision, HAHVAC* sender) {
+    // By default the "temperature" is integer representation of the floating point numbers.
+    // For example: if precision is set to 2, the "25.12" float will be represented as "2512".
+    // If you're using floats you can convert the number to float as shown below.
+    float temperatureFloat = HAUtils::getFloatValue(temperature, precision);
+
+    Serial.print("Target temperature: ");
+    Serial.println(temperatureFloat);
+
+    sender->setTargetTemperature(temperature); // report target temperature back to the HA panel
+}
+
+void onPowerCommand(bool state, HAHVAC* sender) {
+  if (state) {
+    Serial.println("Power on");
+  } else {
+    Serial.println("Power off");
+  }
+}
+
+void onModeCommand(HAHVAC::Mode mode, HAHVAC* sender) {
+    Serial.print("Mode: ");
+    if (mode == HAHVAC::OffMode) {
+        Serial.println("off");
+    } else if (mode == HAHVAC::AutoMode) {
+        Serial.println("auto");
+    } else if (mode == HAHVAC::CoolMode) {
+        Serial.println("cool");
+    } else if (mode == HAHVAC::HeatMode) {
+        Serial.println("heat");
+    } else if (mode == HAHVAC::DryMode) {
+        Serial.println("dry");
+    } else if (mode == HAHVAC::FanOnlyMode) {
+        Serial.println("fan only");
+    }
+
+    sender->setMode(mode); // report mode back to the HA panel
+}
+
+void setup() {
+    Serial.begin(9600);
+    Serial.println("Starting...");
+
+    // Unique ID must be set!
+    byte mac[WL_MAC_ADDR_LENGTH];
+    WiFi.macAddress(mac);
+    device.setUniqueId(mac, sizeof(mac));
+
+    // connect to wifi
+    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+    while (WiFi.status() != WL_CONNECTED) {
+        Serial.print(".");
+        delay(500); // waiting for the connection
+    }
+    Serial.println();
+    Serial.println("Connected to the network");
+
+    // set device's details (optional)
+    device.setName("NodeMCU");
+    device.setSoftwareVersion("1.0.0");
+
+    // assign callbacks (optional)
+    hvac.onTargetTemperatureCommand(onTargetTemperatureCommand);
+    hvac.onPowerCommand(onPowerCommand);
+    hvac.onModeCommand(onModeCommand);
+
+    // configure HVAC (optional)
+    hvac.setName("My HVAC");
+    hvac.setMinTemp(10);
+    hvac.setMaxTemp(30);
+    hvac.setTempStep(0.5);
+
+    // You can set retain flag for the HA commands
+    hvac.setRetain(true);
+
+    // You can choose which modes should be available in the HA panel
+    // hvac.setModes(HAHVAC::OffMode | HAHVAC::CoolMode);
+
+    mqtt.begin(BROKER_ADDR);
+}
+
+void loop() {
+    mqtt.loop();
+
+    if ((millis() - lastTempPublishAt) > 3000) {
+        hvac.setCurrentTemperature(lastTemp);
+        lastTempPublishAt = millis();
+        lastTemp += 0.5;
+    }
+}
